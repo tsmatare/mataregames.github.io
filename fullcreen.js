9385eaadfe5cd37ce4027b565bd13ed84decae8a
mataregames <87947574+mataregames@users.noreply.github.com>
2021-08-10 15:12:14 +0200

    Another update[2]full

--- fullcreen.js
+++ fullcreen.js
@@ -1,28 +1,28 @@
 function toggleFullscreen(elem) {
-elem = elem || document.documentElement;
+  elem = elem || document.documentElement;
 
-if( !document.fullscreenElement && !document.mozFullScreenElement &&
-!document.webkitFullScreenElement && !document.msFullscreenElement){
-if (elem.requestFullScreen) {
-elem.requestFullScreen();
-} else if (elem.msRequestFullscreen) {
-elem.msRequestFullScreen();
-} else if (elem.mozRequestFullScreen) {
-elem.mozRequestFullScreen();
-} else if (elem.webkitRequestFullScreen) {
-elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-}
-}else{
-if (document.exitFullscreen) {
-document.exitFullscreen();
-} else if (document.msExitFullscreen) {
-document.msExitFullscreen();
-} else if (document.mozCancelFullScreen){
-document.mozCancelFullScreen();
-} else if (document.webkitExitFullScreen){
-document.webkitExitFullScreen();
-}
-}
+  if (!document.fullscreenElement && !document.mozFullScreenElement &&
+    !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
 }
 document.getElementById("full_button").addEventListener('click', function(){
 toggleFullscreen();
